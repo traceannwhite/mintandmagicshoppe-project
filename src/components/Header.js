a6265a08082ef52f34bac032526787767f45e22e
traceannwhite <traceannwhite@gmail.com>
2021-08-03 17:23:43 -0500

    Added About page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+// Used this webpage to figure out how to restructure my navigation links as an array, map over them, and target my menu-button to upen and close in React: https://dev.to/nicm42/closing-a-navigation-menu-in-react-8ad
+
 const Header = () => {
+  // const menuButton = "menu-button";
+  // const menu = "menu";
+  // let menuOpen = false;
+  // menuButton.on("click", (event) => {
+  //   if (!menuOpen) {
+  //     menuButton.addClass("open");
+  //     menuOpen = true;
+  //     menu.toggle(250);
+  //   } else {
+  //     menuButton.removeClass("open");
+  //     menuOpen = false;
+  //   }
+  // });
+
+  // set up useState and set the menu state to closed
+  const [menuOpen, setMenuOpen] = useState(false);
+  // update state when menu is open (! overrides )
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  //need an onClick event in the button to set the state
+  //need to add to the link itself so that you'll still go to new page when menu closes
+  // const hideMenu = () => setMenuOpen(false);
+  // const showMenu = () => setMenuOpen(true);
+
+  const navigation = [
+    { link: "/", text: "Home" },
+    { link: "/ShopProducts", text: "Shop Products" },
+    { link: "/MyCart", text: "My Cart" },
+    { link: "/About", text: "About" },
+    { link: "/Contact", text: "Contact" },
+  ];
+
   return (
     <div className="header">
       <div className="icons">
-        <div className="menu-icon">
+        <button className="menu-button" onClick={toggleMenu}>
           <img
             src="https://res.cloudinary.com/dhcagrzcb/image/upload/v1627864926/list_lsoefu.svg"
             alt=""
           ></img>
-        </div>
+          <ul className="menu-links">
+            {navigation.map((nav) => (
+              <li key={nav.text}>
+                <Link to={nav.link}>{nav.text}</Link>
+              </li>
+            ))}
+          </ul>
+        </button>
         <div className="title-img">
           <img
             src="https://res.cloudinary.com/dhcagrzcb/image/upload/v1627866428/Screen_Shot_2021-08-01_at_7.54.50_PM_xqqg7u.png"
             alt=""
           ></img>
         </div>
-        <div className="cauldron-icon">
+        <button className="cauldron-button">
           <Link to="/Cart">
             <img
               src="https://res.cloudinary.com/dhcagrzcb/image/upload/v1627866433/cauldron_vtbv4m.png"
               alt=""
             ></img>
           </Link>
-        </div>
-      </div>
-      <div className="menu">
-        <Link to="/">
-          <div className="menu-item home">Home</div>
-        </Link>
-        <Link to="/ShopProducts">
-          <div className="menu-item products">Shop Products</div>
-        </Link>
-        <Link to="/Cart">
-          <div className="menu-item cart">My Cart</div>
-        </Link>
-        <Link to="/">
-          <div className="menu-item about">About</div>
-        </Link>
-        <Link to="/">
-          <div className="menu-item home">Contact Us</div>
-        </Link>
+        </button>
       </div>
     </div>
   );
---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <p className="one">
+        Mint and Magic Shoppe formed out of the depths of a lightless place
+        amidst a global pandemic when all found it difficult to strive against
+        ones own circumstances. What started as a spark of creativity turned
+        into something more; it bought light back into a dark palce. With the
+        help of others, I realized that to live with abundance one needs to feel
+        a sense of purpose and, for me, a small ounce of control in an
+        unpredictable world. With candles, I could bring to those in their own
+        dark places.
+      </p>
+      <p className="two">
+        The name, Mint and Magic calls on mint to evoke the cleansing and
+        calming effects of it's namesake and magic to fill the depths of one's
+        soul with universal hope and healing.
+      </p>
+    </div>
+  );
+};
+
+export default About;
