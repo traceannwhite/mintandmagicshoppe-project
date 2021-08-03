0856bce647c5cf3759a4c9af3978a175198ae824
traceannwhite <traceannwhite@gmail.com>
2021-08-03 18:55:48 -0500

    added photos to carousel

--- src/components/CustomerReviews.js
+++ src/components/CustomerReviews.js
@@ -10,7 +10,7 @@ import { UncontrolledCarousel } from "reactstrap";
 
 const items = [
   {
-    src: "https://res.cloudinary.com/dhcagrzcb/image/upload/v1627918089/bowl-169435_1920_am7mfk.jpg",
+    src: "https://res.cloudinary.com/dhcagrzcb/image/upload/v1628034032/IMG_4927_isw6vn-_1__wtwe5f.jpg",
     altText: "Slide 1",
     caption:
       '"I love the way the Forest Mint candle looks and smells in my living room!"',
@@ -18,14 +18,14 @@ const items = [
     key: "1",
   },
   {
-    src: "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+    src: "https://res.cloudinary.com/dhcagrzcb/image/upload/v1628004771/IMG_4921_id5ggj_nl3smm.jpg",
     altText: "Slide 2",
     caption: "Slide 2",
     header: "Slide 2 Header",
     key: "2",
   },
   {
-    src: "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+    src: "https://res.cloudinary.com/dhcagrzcb/image/upload/v1628004773/IMG_4929_nclron_yaygtu.jpg",
     altText: "Slide 3",
     caption: "Slide 3",
     header: "Slide 3 Header",
@@ -33,10 +33,13 @@ const items = [
   },
 ];
 
-const CustomerReviews = () => <UncontrolledCarousel items={items} />;
+const CustomerReviews = () => (
+  <div className="carousel">
+    <UncontrolledCarousel items={items} />
+  </div>
+);
 
 export default CustomerReviews;
 
-
 // PROBLEM: Had initial trouble with adding url to src
-// SOLUTION: Won't except files in HEIC format. Must convert to JPG before inputting. 
+// SOLUTION: Won't except files in HEIC format. Must convert to JPG before inputting.
