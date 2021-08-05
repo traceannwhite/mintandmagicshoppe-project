ce2d5e048303548ccf7b513c9006f006480ce83f
traceannwhite <traceannwhite@gmail.com>
2021-08-05 13:32:55 -0500

    Update

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,7 +2,6 @@ import React from "react";
 import Button from "./Button";
 
 const Products = (props) => {
-  console.log("Products", props);
   const loaded = () => {
     return (
       <div className="product-container">
